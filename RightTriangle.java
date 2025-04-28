d84de5bb44350976eff46405beda5476225312a5
SandhyaRajput040 <sandhyarajput0405853@gmail.com>
2025-04-28 12:29:58 +0530

    file updated

--- RightTriangle.java
+++ RightTriangle.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class RightTriangle {
     public static void main(String[] args) {
-        
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("Enetr a Number :");
+        int num = scanner.nextInt();
+        for(int i =1; i<=num; i++){
+            for(int k=i; k<=num; k++ ){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
