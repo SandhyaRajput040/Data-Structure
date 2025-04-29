d5f5a407d8961c9c58d9132824e723d270182828
SandhyaRajput040 <sandhyarajput0405853@gmail.com>
2025-04-29 14:11:49 +0530

    Pattern

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class DiamondPattern {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int num = scanner.nextInt();
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=num; j++){
+             if(i+j>=7 && j-i<6 && i+j<=17 && i-j<=5 ) {
+                System.out.print("*");
+                }else{
+                 System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
